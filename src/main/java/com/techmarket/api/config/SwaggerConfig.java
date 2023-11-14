f0933c9ee25e695fafee1615b2f9e84b40f2cdc5
TranQuangThoi <tranquangthoi420632@gmail.com>
2023-11-14 21:00:50 +0700

    add authentizaion into sswaagger

--- src/main/java/com/techmarket/api/config/SwaggerConfig.java
+++ src/main/java/com/techmarket/api/config/SwaggerConfig.java
@@ -8,12 +8,15 @@ import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.AuthorizationScope;
+import springfox.documentation.service.SecurityReference;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 import java.util.Arrays;
 import java.util.HashSet;
+import java.util.List;
 
 @Configuration
 @EnableSwagger2
@@ -30,12 +33,30 @@ public class SwaggerConfig {
                 .select().apis(RequestHandlerSelectors.basePackage("com.techmarket.api.controller"))
                 .paths(PathSelectors.any())
                 .build()
-                .apiInfo(apiInfo());
+                .apiInfo(apiInfo())
+                .securitySchemes(Arrays.asList(apiKey()))
+                .securityContexts(Arrays.asList(securityContext()));
     }
     private ApiInfo apiInfo() {
         return new ApiInfoBuilder().title("TECH MARKET API")
                 .description("API tech")
                 .version("1.0.0")
                 .build();
+    }
+    private springfox.documentation.spi.service.contexts.SecurityContext securityContext() {
+        return springfox.documentation.spi.service.contexts.SecurityContext.builder()
+                .securityReferences(defaultAuth())
+                .build();
+    }
+
+    private List<SecurityReference> defaultAuth() {
+        AuthorizationScope authorizationScope = new AuthorizationScope("global", "accessEverything");
+        AuthorizationScope[] authorizationScopes = new AuthorizationScope[1];
+        authorizationScopes[0] = authorizationScope;
+        return Arrays.asList(new SecurityReference("JWT", authorizationScopes));
+    }
+
+    private springfox.documentation.service.ApiKey apiKey() {
+        return new springfox.documentation.service.ApiKey("JWT", "Authorization", "header");
     }
 }
